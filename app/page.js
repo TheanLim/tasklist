a7a4a5e2e7d2d1df5cccb9c1edcdef0ccb9828c2
Thean Lim <limxx431@umn.edu>
2024-09-18 01:47:23 -0700

    Minor Cleanup

--- app/page.js
+++ app/page.js
@@ -57,7 +57,6 @@ const Home = () => {
 
   return (
     <div className=' to-primary'>
-      <div>{searchQuery}</div>
       {/* Tag buttons */}
       <div className='flex gap-1 m-2'>
         <button className={`btn ${selectedTag === null ? 'btn-primary' : 'btn-ghost'} btn-sm`} onClick={() => setSelectedTag(null)}>All</button>
